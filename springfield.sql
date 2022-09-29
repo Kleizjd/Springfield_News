a2a4e6068c5b74c07a882cad57bfa34fccc0179e
Kleizjd <jose.jdgo98@gmail.com>
2022-09-29 14:39:42 -0500

    db

--- springfield.sql
+++ springfield.sql
@@ -34,31 +34,25 @@ CREATE TABLE IF NOT EXISTS `noticias` (
   `categoria` varchar(50) NOT NULL DEFAULT '0',
   `estado` char(1) NOT NULL,
   `descripcion` varchar(250) NOT NULL,
+  `portada` varchar(50) DEFAULT NULL,
+  `datecreated` datetime DEFAULT current_timestamp(),
+  `ruta` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=12345647 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12345660 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla springfield.noticias: ~19 rows (aproximadamente)
-INSERT INTO `noticias` (`id`, `titulo`, `categoria`, `estado`, `descripcion`) VALUES
-	(1, 'Nescafe', 'cultura', 'A', 'modifica'),
-	(2, 'cafe aguila roja granulad', 'deporte', 'A', 'jose vamos'),
-	(3, 'bolsa cafe sello rojo', 'economia', 'A', 'desarrolla'),
-	(4, 'vidrio cafe aroma', 'deporte', 'A', 'bastante'),
-	(5, 'vidrio cafe legal descafe', '1', 'A', 'yes very well'),
-	(7, 'vidrio cafe  dolca nescafe', '1', 'A', ''),
-	(8, 'vidrio cafe legal', '1', 'A', ''),
-	(9, 'Cafe soluble Nescafe Tasters c', '1', 'A', 'barato'),
-	(10, 'vidrio cafe colcate descafeina', '1', 'A', ''),
-	(11, 'plastico crama para cafe nestle', '1', 'A', ''),
-	(12, 'carton crema para cafe vainill', '1', 'A', ''),
-	(13, 'crema para cafe aguila roja no', '1', 'A', ''),
-	(14, 'crema  para cafe aroma', '1', 'A', ''),
-	(15, 'plastico crema para cafe colcafe', '1', 'A', ''),
-	(123, '123', '', 'A', ''),
-	(456, 'carne', '', 'A', ''),
-	(886, '8', '', 'A', ''),
-	(3443, '434', '', 'A', '5'),
-	(12366, 'jugo de uva', '', '', ''),
-	(12345646, 'as', 'SD', 'A', 'wqe');
+-- Volcando datos para la tabla springfield.noticias: ~10 rows (aproximadamente)
+INSERT INTO `noticias` (`id`, `titulo`, `categoria`, `estado`, `descripcion`, `portada`, `datecreated`, `ruta`) VALUES
+	(2, 'cafe aguila roja granulad', 'deporte', 'A', 'jose vamos', 'img_86bbb5b0a0d08f7b00ede09bb7ca2065.jpg', '2022-09-29 14:37:29', NULL),
+	(3, 'bolsa cafe sello rojo', 'economia', 'A', 'desarrolla', 'img_86bbb5b0a0d08f7b00ede09bb7ca2065.jpg', '2022-09-29 14:37:32', NULL),
+	(4, 'vidrio cafe aroma', 'deporte', 'A', 'bastante', 'img_d8f20ac16dffcd2c6a275dc7b76e0056.jpg', '2022-09-29 14:37:33', NULL),
+	(5, 'vidrio cafe legal descafe', '1', 'A', 'yes very well', 'img_ab4a831d138ff790e95133a788594777.jpg', '2022-09-29 14:37:35', NULL),
+	(7, 'vidrio cafe  dolca nescafe', '1', 'A', '', 'portada_noticia.png', '2022-09-29 14:37:38', NULL),
+	(8, 'vidrio cafe legal', '1', 'A', '', 'img_86bbb5b0a0d08f7b00ede09bb7ca2065.jpg', '2022-09-29 14:37:40', NULL),
+	(9, 'Cafe soluble Nescafe Tasters c', '1', 'A', 'barato', 'img_86bbb5b0a0d08f7b00ede09bb7ca2065.jpg', '2022-09-29 14:37:42', NULL),
+	(10, 'vidrio cafe colcate descafeina', '1', 'A', '', 'img_86bbb5b0a0d08f7b00ede09bb7ca2065.jpg', '2022-09-29 14:37:43', NULL),
+	(13, 'crema para cafe aguila roja no', '1', 'A', '', 'img_86bbb5b0a0d08f7b00ede09bb7ca2065.jpg', '2022-09-29 14:37:44', NULL),
+	(15, 'plastico crema para cafe colcafe', '1', 'A', '', 'img_86bbb5b0a0d08f7b00ede09bb7ca2065.jpg', '2022-09-29 14:37:47', NULL),
+	(12345659, 'hey', '3', '', 'jey', 'img_83533f736464a6f70354cbad8e3e907a.jpg', '2022-09-29 14:32:33', 'hey');
 
 -- Volcando estructura para tabla springfield.perfiles
 CREATE TABLE IF NOT EXISTS `perfiles` (
@@ -66,7 +60,7 @@ CREATE TABLE IF NOT EXISTS `perfiles` (
   `nombre` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Volcando datos para la tabla springfield.perfiles: ~2 rows (aproximadamente)
+-- Volcando datos para la tabla springfield.perfiles: ~3 rows (aproximadamente)
 INSERT INTO `perfiles` (`id`, `nombre`) VALUES
 	(1, 'administrador'),
 	(2, 'lector'),
@@ -78,7 +72,7 @@ CREATE TABLE IF NOT EXISTS `preguntas` (
   `pregunta` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Volcando datos para la tabla springfield.preguntas: ~4 rows (aproximadamente)
+-- Volcando datos para la tabla springfield.preguntas: ~3 rows (aproximadamente)
 INSERT INTO `preguntas` (`id`, `pregunta`) VALUES
 	(1, 'cual fue el nombre de su primera mascota?'),
 	(2, 'lugar de nacimiento?'),
@@ -97,9 +91,9 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `id_pregunta` int(11) DEFAULT NULL,
   `respuesta` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id_usuario`) USING BTREE
-) ENGINE=MyISAM AUTO_INCREMENT=50 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=51 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla springfield.usuarios: 20 rows
+-- Volcando datos para la tabla springfield.usuarios: 21 rows
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
 INSERT INTO `usuarios` (`id_usuario`, `nombre`, `apellido`, `password`, `email`, `estado_usuario`, `rolid`, `imagen_usuario`, `id_pregunta`, `respuesta`) VALUES
 	(1, '   José Daniel', '   Grijalba', '$2y$10$An1ghzpGGYth9vnvA6CHveNqNWNqLtBDzMv.QOLhaIZkpeZsYR6HC', 'jose.jdgo97@gmail.com', 'A', 1, '2-1.jpg', 1, NULL),
@@ -121,7 +115,8 @@ INSERT INTO `usuarios` (`id_usuario`, `nombre`, `apellido`, `password`, `email`,
 	(43, '1', '1', '$2y$10$u1AyCq/KPvkBznR95hodPOcWO3eNotAyLQS.7Qhrb5jkOJw.k7q52', 'andres@hotmail.com', 'A', 2, '', NULL, NULL),
 	(44, 'pablo', 'neruda', '$2y$10$gDtToYDxzH3HlnIH3iVRzefBnKoFCO8Ag.C4Ffx4xLjNUeAslbc3i', 'pablo@gmai.com', 'A', 2, '', 3, NULL),
 	(45, 'andres', 'perez', '$2y$10$5UpZfl5EtSsYPqCkf.0s5OhyfgmUTFbiZpCZ/UbCoZFL2Kp4T20mC', 'perez@14perez.com', 'A', 2, '', 2, NULL),
-	(48, 'camilo', 'garzon', '$2y$10$.DgeiyX3xksaR.4e2uVPZueO9K0I9Wv0AZQhYlJdrcfPiL0cVocta', 'camilo@14perez.com', 'A', 2, '', 1, 'lucas');
+	(48, 'camilo', 'garzon', '$2y$10$.DgeiyX3xksaR.4e2uVPZueO9K0I9Wv0AZQhYlJdrcfPiL0cVocta', 'camilo@14perez.com', 'A', 2, '', 1, 'lucas'),
+	(50, 'mario', 'bros', '$2y$10$gQ7viBqhZY/X4SFWuGYzee5ttX46hHHwE6UeAe3KF0RPRpBlvLm4e', 'mario@mail.com', 'A', 2, '', 1, 'lucas');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
