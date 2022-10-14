76bd00d3841b9a3de71cf93eb5adb5f4cd20ba62
Kleizjd <jose.jdgo98@gmail.com>
2022-10-14 13:18:09 -0500

    bd final

--- springfield.sql
+++ springfield.sql
@@ -34,18 +34,21 @@ CREATE TABLE IF NOT EXISTS `comentario` (
   `id_noticia` int(11) DEFAULT NULL,
   `comentario` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=154 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=159 DEFAULT CHARSET=utf8mb4;
 
--- Volcando datos para la tabla springfield.comentario: ~8 rows (aproximadamente)
+-- Volcando datos para la tabla springfield.comentario: ~7 rows (aproximadamente)
 INSERT INTO `comentario` (`id`, `email`, `id_noticia`, `comentario`) VALUES
 	(1, 'dianaaristizabal@gmail.com', 7, 'come on'),
 	(2, 'dianaaristizabal@gmail.com', 8, 'de que'),
-	(3, 'jose.jdgo97@gmail.com', 7, 'claro'),
 	(4, 'jose.jdgo97@gmail.com', 10, 'el mejor que Dios puso'),
 	(5, 'jose.jdgo97@gmail.com', 11, 'bien'),
 	(6, 'naruto@mail.com', 6, 'eso si'),
 	(7, 'naruto@mail.com', 7, 'tambien'),
-	(8, 'naruto@mail.com', 8, 'yo si creo');
+	(8, 'naruto@mail.com', 8, 'yo si creo'),
+	(154, 'dianaaristizabal@gmail.com', 1, 'hey'),
+	(155, 'jose.jdgo97@gmail.com', 1, 'yo'),
+	(157, 'juandgo1997@gmail.com', 1, 'mario bros'),
+	(158, 'naruto@mail.com', 1, 'sera');
 
 -- Volcando estructura para tabla springfield.noticias
 CREATE TABLE IF NOT EXISTS `noticias` (
@@ -60,7 +63,7 @@ CREATE TABLE IF NOT EXISTS `noticias` (
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=12345680 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla springfield.noticias: ~8 rows (aproximadamente)
+-- Volcando datos para la tabla springfield.noticias: ~9 rows (aproximadamente)
 INSERT INTO `noticias` (`id`, `titulo`, `categoria`, `estado`, `descripcion`, `portada`, `datecreated`, `ruta`) VALUES
 	(1, 'Cafe mata', '4', 'A', 'barato', 'img_86bbb5b0a0d08f7b00ede09bb7ca2065.jpg', '2022-09-29 14:37:42', NULL),
 	(2, 'jovenes', '2', '', 'murieron portada al mar', 'img_2cbec034433250cf3864cc563cb4b4ae.jpg', '2022-09-29 16:54:20', 'jovenes'),
@@ -95,6 +98,29 @@ INSERT INTO `preguntas` (`id`, `pregunta`) VALUES
 	(2, 'lugar de nacimiento?'),
 	(3, 'color favorito?');
 
+-- Volcando estructura para tabla springfield.rating
+CREATE TABLE IF NOT EXISTS `rating` (
+  `email` varchar(50) DEFAULT NULL,
+  `id_noticia` int(11) DEFAULT NULL,
+  `calificacion` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Volcando datos para la tabla springfield.rating: ~1 rows (aproximadamente)
+INSERT INTO `rating` (`email`, `id_noticia`, `calificacion`) VALUES
+	('jose.jdgo97@gmail.com', 8, 5),
+	('jose.jdgo97@gmail.com', 7, NULL),
+	('naruto@mail.com', 8, 3),
+	('naruto@mail.com', 1, 1),
+	('naruto@mail.com', 2, 4),
+	('naruto@mail.com', 4, 4),
+	('naruto@mail.com', 3, 3),
+	('dianaaristizabal@gmail.com', 8, 5),
+	('dianaaristizabal@gmail.com', 2, 5),
+	('dianaaristizabal@gmail.com', 3, 3),
+	('juandgo1997@gmail.com', 8, 5),
+	('juandgo1997@gmail.com', 4, 2),
+	('juandgo1997@gmail.com', 1, 3);
+
 -- Volcando estructura para tabla springfield.reaccion
 CREATE TABLE IF NOT EXISTS `reaccion` (
   `email` varchar(50) DEFAULT NULL,
@@ -112,7 +138,14 @@ INSERT INTO `reaccion` (`email`, `id_noticia`) VALUES
 	('jose.jdgo97@gmail.com', 2),
 	('naruto@mail.com', 7),
 	('dianaaristizabal@gmail.com', 7),
-	('dianaaristizabal@gmail.com', 4);
+	('dianaaristizabal@gmail.com', 4),
+	('juandgo1997@gmail.com', 8),
+	('jose.jdgo97@gmail.com', 8),
+	('jose.jdgo97@gmail.com', 4),
+	('juandgo1997@gmail.com', 7),
+	('juandgo1997@gmail.com', 1),
+	('dianaaristizabal@gmail.com', 8),
+	('naruto@mail.com', 1);
 
 -- Volcando estructura para tabla springfield.usuarios
 CREATE TABLE IF NOT EXISTS `usuarios` (
@@ -127,18 +160,19 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `id_pregunta` int(11) DEFAULT NULL,
   `respuesta` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id_usuario`) USING BTREE
-) ENGINE=MyISAM AUTO_INCREMENT=72 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=73 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla springfield.usuarios: 7 rows
+-- Volcando datos para la tabla springfield.usuarios: 8 rows
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
 INSERT INTO `usuarios` (`id_usuario`, `nombre`, `apellido`, `password`, `email`, `estado_usuario`, `rolid`, `imagen_usuario`, `id_pregunta`, `respuesta`) VALUES
-	(1, 'José Daniel', 'Grijalba', '$2y$10$bTCrFmUyt7d9NuAU5SQiRuVtRIcPOiqhluQJ.a2uZ.QANKndigrTy', 'jose.jdgo97@gmail.com', 'A', 1, 'imagen-jose-1.jpg', 1, 'lucas'),
-	(2, 'Juan David', 'Grijalba', '$2y$10$cpogeAdHRyRBkotAeQ3nS.tck5whOGT0VAgs6CcPda3/iN.k44qIS', 'juandgo1997@gmail.com', 'A', 3, '2-2.jpg', 1, 'simon'),
-	(4, '    Diana', '    Aristizabal', '$2y$10$TNQULbSAcW1Jojir7xbW/OKwAPUxAd3tE3Q.ePTsEzi.5qYFk2NxC', 'dianaaristizabal@gmail.com', 'A', 3, 'diana-diana-4.png', 1, 'cielo'),
-	(9, 'arley', 'd', '$2y$10$/AWJxM/m6Z6cm1YaUAvtJ.IRimLpiDUm.eMjrriPXtdwvHxJUG1BG', 'arley@gmail.com', 'A', 3, '2-32.jpg', 1, 'sabe'),
-	(71, 'neji', 'asd', '$2y$10$eAat5ki8NiOPW2gTVfCYAuPztMmPPPgZYlc5vEtlsA0rIYYb1Gt.y', 'jos.jo97@gmail.com', 'A', 2, '', 1, 'anime'),
-	(58, 'sasuke', 'uchija', '$2y$10$z/ssTu0goK8spUIrjug0luSTPJjJ5bxy/nCd7rjPQ.JKMBXsV4moq', 'sasuke@gmail.com', 'A', 2, '', 1, 'lucas'),
-	(14, 'naruto', 'uzumaki', '$2y$10$y9CGVfte4fLUuwGwAWYyyeS/qttSy1EtDcSiFWqGqWBTSnfKATBPy', 'naruto@mail.com', 'A', 2, 'naruto-14.jpg', 1, 'lucas');
+	(1, 'José Daniel', 'Grijalba', '$2y$10$bTCrFmUyt7d9NuAU5SQiRuVtRIcPOiqhluQJ.a2uZ.QANKndigrTy', 'jose.jdgo97@gmail.com', 'A', 1, 'jose.jpg', 1, 'lucas'),
+	(2, 'Juan David', 'Grijalba', '$2y$10$bTCrFmUyt7d9NuAU5SQiRuVtRIcPOiqhluQJ.a2uZ.QANKndigrTy', 'juandgo1997@gmail.com', 'A', 3, 'juan.jpg', 1, 'simon'),
+	(4, 'Diana', 'Aristizabal', '$2y$10$bTCrFmUyt7d9NuAU5SQiRuVtRIcPOiqhluQJ.a2uZ.QANKndigrTy', 'diana.aristizabal@gmail.com', 'A', 3, 'diana.png', 1, 'cielo'),
+	(9, 'Arley', 'Castano', '$2y$10$/AWJxM/m6Z6cm1YaUAvtJ.IRimLpiDUm.eMjrriPXtdwvHxJUG1BG', 'arley@gmail.com', 'A', 3, 'Arley.jpg', 1, 'sabe'),
+	(71, 'Neji', 'hyuga', '$2y$10$eAat5ki8NiOPW2gTVfCYAuPztMmPPPgZYlc5vEtlsA0rIYYb1Gt.y', 'neji@gmail.com', 'A', 2, 'naruto_neji_hyuga-71.jpg', 1, 'anime'),
+	(72, 'Eren', 'Yeager', '$2y$10$eAat5ki8NiOPW2gTVfCYAuPztMmPPPgZYlc5vEtlsA0rIYYb1Gt.y', 'eren@gmail.com', 'A', 3, 'eren_yeager-72.jpg', NULL, NULL),
+	(58, 'Sasuke', 'uchija', '$2y$10$bTCrFmUyt7d9NuAU5SQiRuVtRIcPOiqhluQJ.a2uZ.QANKndigrTy', 'sasuke@gmail.com', 'A', 2, 'Sasuke-58.jpg', 1, 'lucas'),
+	(14, 'Naruto', 'uzumaki', '$2y$10$y9CGVfte4fLUuwGwAWYyyeS/qttSy1EtDcSiFWqGqWBTSnfKATBPy', 'naruto@mail.com', 'A', 2, 'naruto-14.jpg', 1, 'lucas');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
